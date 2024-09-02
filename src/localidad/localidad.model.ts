3b23c058207224cd55464c21d28455950ee0dd94
Jiun Ming Hsu <hsujm219@gmail.com>
2024-09-01 22:13:07 -0300

    update

--- src/localidad/localidad.model.ts
+++
@@ -1,8 +0,0 @@
-import { Provincia } from '../provincia/provincia.model';
-
-export interface Localidad {
-    // id: number;
-    nombre: string;
-    provincia: Provincia;
-    etiqueta: string;
-}
--- src/puntoDonacion/puntoDonacion.model.ts
+++ src/puntoDonacion/puntoDonacion.model.ts
@@ -1,9 +1,19 @@
-import { Localidad } from '../localidad/localidad.model';
+export interface Provincia {
+    nombre: string;
+    etiqueta: string;
+}
+
+export interface Localidad {
+    nombre: string;
+    provincia: Provincia;
+    etiqueta: string;
+}
 
 export interface PuntoDonacion {
-    // id: number;
     nombre: string;
     localidad: Localidad;
+    latitud: Number;
+    longitud: Number;
     calle: string;
     altura: Number;
 }
