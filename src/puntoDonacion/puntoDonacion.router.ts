645ad91c7a3e07b15a0a62dc9861376ffcce2bf6
Jiun Ming Hsu <hsujm219@gmail.com>
2024-09-24 01:54:58 -0300

    documentacion con swagger-jsdoc

--- src/puntoDonacion/puntoDonacion.router.ts
+++ src/puntoDonacion/puntoDonacion.router.ts
@@ -3,7 +3,130 @@ import controller from './puntoDonacion.controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     PuntoDonacion:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: UUID del punto de donación.
+ *         nombre:
+ *           type: string
+ *           description: Nombre del punto de donación.
+ *         calle:
+ *           type: string
+ *           description: Calle del punto de donación.
+ *         altura:
+ *           type: number
+ *           description: Altura del punto de donación.
+ *         latitud:
+ *           type: string
+ *           description: Latitud del punto de donación.
+ *         longitud:
+ *           type: string
+ *           description: Longitud del punto de donación.
+ *         localidad:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: number
+ *               description: ID de la localidad.
+ *             nombre:
+ *               type: string
+ *               description: Nombre de la localidad.
+ *             etiqueta:
+ *               type: string
+ *               description: Etiqueta de la localidad.
+ *         provincia:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: number
+ *               description: ID de la localidad.
+ *             nombre:
+ *               type: string
+ *               description: Nombre de la localidad.
+ *             etiqueta:
+ *               type: string
+ *               description: Etiqueta de la localidad.
+ *       example:
+ *         id: 3a7502ae-7185-11ef-98ce-3c7c3f266695
+ *         nombre: punto2
+ *         calle: Av. Cordoba
+ *         altura: 4857
+ *         latitud: 5468968245
+ *         longitud: 5643212543
+ *         provincia:
+ *           id: 1
+ *           nombre: Ciudad Autonoma de Buenos Aires
+ *           etiqueta: caba
+ *         localidad:
+ *           id: 1
+ *           nombre: Almagro
+ *           etiqueta: almagro
+ *
+ *
+ */
+
+/**
+ * @swagger
+ * /api/punto-donacion:
+ *   get:
+ *     tags:
+ *      - PuntoDonacion
+ *     summary: Retorna una lista de puntos de donación.
+ *     description: Obtiene los puntos de donación por provincia y localidad.
+ *     parameters:
+ *      - in: query
+ *        name: provincia
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: Nombre de la provincia (etiqueta).
+ *      - in: query
+ *        name: localidad
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: Nombre de la localidad (etiqueta).
+ *     responses:
+ *       200:
+ *         description: Retorna una lista de puntos de donación.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/PuntoDonacion'
+ */
 router.get('/', controller.getByProvinciaAndLocalidad);
+
+/**
+ * @swagger
+ * /api/punto-donacion/{id}:
+ *   get:
+ *     tags:
+ *      - PuntoDonacion
+ *     summary: Retorna un punto de donación.
+ *     description: Obtiene un puntos de donación por ID.
+ *     parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: UUID del punto de donación.
+ *     responses:
+ *       200:
+ *         description: Retorna un punto de donación.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/PuntoDonacion'
+ */
 router.get('/:id', controller.getById);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import { ServerConfig } from './config/server.config';
 import puntoDonacionRouter from './puntoDonacion/puntoDonacion.router';
 import dataSource from './db/datasource';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 const main = async () => {
     try {
@@ -15,8 +17,28 @@ const main = async () => {
 
         app.disable('x-powered-by');
 
+        const options = {
+            definition: {
+                openapi: '3.0.0',
+                info: {
+                    title: 'Recomendación de Donación API',
+                    version: '1.0.0',
+                    description: 'Servicio 1 del TPA de Diseño de Sistemas',
+                },
+                servers: [
+                    {
+                        url: `http://${host}:${port}`,
+                    },
+                ],
+            },
+            apis: ['./**/*.router.ts'],
+        };
+
+        const swaggerSpec = swaggerJSDoc(options);
+
         await dataSource.initialize();
 
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
         app.use(morgan('dev'));
@@ -25,7 +47,7 @@ const main = async () => {
         app.use('/test', () => {
             console.log('test');
         });
-        app.use('/api/puntoDonacion', puntoDonacionRouter);
+        app.use('/api/punto-donacion', puntoDonacionRouter);
         app.use((_req, res) => {
             res.status(404).json({ message: 'not found' });
         });
